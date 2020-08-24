4dcb6b8e8669ffa7e4b2153b2f6044f6107d805c
Jaewoong Lee <230@fumire.moe>
2020-08-24 21:02:29 +0900

    Add column

--- jwlee230/Program/Python/make_metadata.py
+++ jwlee230/Program/Python/make_metadata.py
@@ -1,10 +1,10 @@
 import sys
 
-print("#SampleID", "BarcodeSequence", "LinkPrimerSequence", "Group", "BodySite", "Description", sep="\t")
-print("#q2:types", "categorical", "categorical", "categorical", "categorical", "categorical", sep="\t")
+print("#SampleID", "BarcodeSequence", "LinkPrimerSequence", "GroupAndSite", "Group", "BodySite", "Description", sep="\t")
+print("#q2:types", "categorical", "categorical", "categorical", "categorical", "categorical", "categorical", sep="\t")
 
 group = {"C": "Cavity", "N": "Normal"}
 site = {"S": "Saliva", "P": "Plaque"}
 
 for sample in sys.argv[1:]:
-    print(sample, "", "", group[sample[0]], site[sample[2]], "", sep="\t")
+    print(sample, "", "", group[sample[0]] + site[sample[2]], group[sample[0]], site[sample[2]], "", sep="\t")
